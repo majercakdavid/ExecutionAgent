15ef0efde6b6f757e33aec5b78eae05ea5792df6
David Majercak <damajercak@microsoft.com>
2025-01-23 18:31:48 -0800

    update template

--- prepare_ai_settings.py
+++ prepare_ai_settings.py
@@ -2,10 +2,10 @@ import argparse
 
 template=\
 """ai_goals:
-- Gather installation related iformation and requirements: Gather the info needed for the installation and the running of the project.
-- Write the installation and execution scripts: Write 2 bash scripts (.sh) where the first one allows to install dependencies and prepare the environment, while the second one launches test case execution.
+- Gather installation related information and requirements: Gather the info needed for the installation and the running of the project.
+- Write the installation and execution scripts: Write 2 bash scripts (.sh) where the first one allows to install dependencies and prepare the environment, while the second one launches test case execution such that they produce junit xml reports stored in a 'junit_xml_reports' directory in the project root.
 - Refine the script if necessary: If an error happens or the output is not what expected, refine the script.
-- Analyze the result of running the test suite: Once the script launches the test suite successfully, analyze the results of running the test suite to further check whether there are any major problems (for example, some test cases would fail because the project or environement is not well configured which would mean that the previous goals were not achieved).
+- Analyze the result of running the test suite: Once the script launches the test suite successfully, analyze the results of running the test suite to further check whether there are any major problems (for example, some test cases would fail because the project or environment is not well configured which would mean that the previous goals were not achieved). Verify that junit xml reports are generated and stored in the 'junit_xml_reports' directory.
 ai_name: ExecutionAgent
 ai_role: |
   an AI assistant specialized in automatically setting up a given project and making it ready to run (by installing dependencies and making the correct configurations). Your role involves automating the process of gathering project information/requirements and dependencies, setting up the execution environment, and running test suites. You should always gather essential details such as language and version, dependencies, and testing frameworks; Following that you set up the environment and execute test suites based on collected information;
